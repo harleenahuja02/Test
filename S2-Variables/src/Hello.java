3e36e1ef3e81b1b55f2b4fe169c13f0b84e7b203
DELL <DELL@HARLEEN>
2021-06-04 00:21:10 +0530

    Details and Reason for making changes : First commit

---
+++ S2-Variables/src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int val=1,val1=2, val2=(2*2)-val1;
+		System.out.println("val");
+		System.out.println(val);
+		System.out.println(val1);
+		System.out.println(val2);
+	}
+
+}
